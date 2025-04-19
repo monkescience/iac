42ffb759e21bfd1ab2796a13d637d804a3823141
ValentinLutz <allround.email@gmail.com>
2025-04-19 17:14:14 +0200

    feat: update AWS SSO account assignments for multiple environments

--- components/sso/base/main.tf
+++ components/sso/base/main.tf
@@ -10,9 +10,15 @@ resource "aws_ssoadmin_managed_policy_attachment" "admin" {
   permission_set_arn = aws_ssoadmin_permission_set.admin.arn
 }
 
-resource "aws_ssoadmin_account_assignment" "root_admin" {
+resource "aws_ssoadmin_account_assignment" "admin" {
+  for_each = tomap({
+    "monke-root" = "489721517942"
+    "monke-dev"  = "387105013966"
+    "monke-prod" = "978150176582"
+  })
+
   instance_arn       = tolist(data.aws_ssoadmin_instances.sso_instance.arns)[0]
-  target_id          = "489721517942"
+  target_id          = each.value
   target_type        = "AWS_ACCOUNT"
   permission_set_arn = aws_ssoadmin_permission_set.admin.arn
   principal_type     = "GROUP"
