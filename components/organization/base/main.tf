dd329656fb67bcbd946cfc151c111ea3e1451287
ValentinLutz <allround.email@gmail.com>
2025-04-19 16:55:10 +0200

    feat: add AWS Organizations accounts for development and production environments

--- components/organization/base/main.tf
+++ components/organization/base/main.tf
@@ -4,3 +4,13 @@ resource "aws_organizations_organization" "org" {
   ]
   feature_set = "ALL"
 }
+
+resource "aws_organizations_account" "dev" {
+  name  = "monke-dev"
+  email = "allround.email+monke-dev@gmail.com"
+}
+
+resource "aws_organizations_account" "prod" {
+  name  = "monke-prod"
+  email = "allround.email+monke-prod@gmail.com"
+}
