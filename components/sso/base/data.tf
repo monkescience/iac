cad9af67e5856d1c1314e909e2cae4e153bf16d5
ValentinLutz <allround.email@gmail.com>
2025-04-19 16:43:54 +0200

    feat: add AWS SSO configuration and identity management resources

---
+++ components/sso/base/data.tf
@@ -0,0 +1,3 @@
+// Needs to be created manually in AWS Console
+// https://docs.aws.amazon.com/res/latest/ug/sso-idc.html
+data "aws_ssoadmin_instances" "sso_instance" {}
---
+++ components/sso/base/main.tf
@@ -0,0 +1,47 @@
+resource "aws_ssoadmin_permission_set" "admin" {
+  instance_arn     = tolist(data.aws_ssoadmin_instances.sso_instance.arns)[0]
+  name             = "admin"
+  session_duration = "PT2H"
+}
+
+resource "aws_ssoadmin_managed_policy_attachment" "admin" {
+  instance_arn       = tolist(data.aws_ssoadmin_instances.sso_instance.arns)[0]
+  managed_policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  permission_set_arn = aws_ssoadmin_permission_set.admin.arn
+}
+
+resource "aws_ssoadmin_account_assignment" "root_admin" {
+  instance_arn       = tolist(data.aws_ssoadmin_instances.sso_instance.arns)[0]
+  target_id          = "489721517942"
+  target_type        = "AWS_ACCOUNT"
+  permission_set_arn = aws_ssoadmin_permission_set.admin.arn
+  principal_type     = "GROUP"
+  principal_id       = aws_identitystore_group.admin.group_id
+}
+
+resource "aws_identitystore_group" "admin" {
+  identity_store_id = tolist(data.aws_ssoadmin_instances.sso_instance.identity_store_ids)[0]
+  display_name      = "admin"
+}
+
+resource "aws_identitystore_group_membership" "vlutz" {
+  identity_store_id = tolist(data.aws_ssoadmin_instances.sso_instance.identity_store_ids)[0]
+  group_id          = aws_identitystore_group.admin.group_id
+  member_id         = aws_identitystore_user.vlutz.user_id
+}
+
+resource "aws_identitystore_user" "vlutz" {
+  identity_store_id = tolist(data.aws_ssoadmin_instances.sso_instance.identity_store_ids)[0]
+  display_name      = "Valentin Lutz"
+  user_name         = "vlutz"
+
+  name {
+    given_name  = "Valentin"
+    family_name = "Lutz"
+  }
+
+  emails {
+    primary = true
+    value   = "allround.email@gmail.com"
+  }
+}
