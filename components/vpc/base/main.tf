b1b5beb6c6374053cf977cf34df266eabe9cc0f6
ValentinLutz <allround.email@gmail.com>
2025-04-26 20:16:36 +0200

    feat: enable network address usage metrics for VPC configuration

--- components/vpc/base/main.tf
+++ components/vpc/base/main.tf
@@ -1,8 +1,9 @@
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr_block
 
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+  enable_dns_support                   = true
+  enable_dns_hostnames                 = true
+  enable_network_address_usage_metrics = true
 
   tags = {
     Name = module.vpc_name.name
