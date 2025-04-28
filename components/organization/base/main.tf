9e1a1ea480230c9ab6a61843a5c3afa1d3fb0b68
ValentinLutz <allround.email@gmail.com>
2025-04-28 21:07:23 +0200

    feat: add budget resource with notifications for cost management

--- components/organization/base/main.tf
+++ components/organization/base/main.tf
@@ -14,3 +14,27 @@ resource "aws_organizations_account" "prod" {
   name  = "monke-prod"
   email = "allround.email+monke-prod@gmail.com"
 }
+
+resource "aws_budgets_budget" "org" {
+  name         = module.budget_name.name
+  budget_type  = "COST"
+  limit_amount = "50"
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = ["allround.email+monke-budget@gmail.com"]
+  }
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = ["allround.email+monke-budget@gmail.com"]
+  }
+}
--- components/organization/base/modules.tf
+++ components/organization/base/modules.tf
@@ -4,3 +4,12 @@ module "account_check" {
   region      = var.region
   environment = var.environment
 }
+
+module "budget_name" {
+  source = "../../../modules/name"
+
+  project     = var.project
+  region      = var.region
+  environment = var.environment
+  name        = "budget"
+}
