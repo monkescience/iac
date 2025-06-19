958acb515cb1375f61b6c06f91787405aaa205df
ValentinLutz <allround.email@gmail.com>
2025-06-19 16:20:29 +0200

    feat: add IAM roles and policies for cert-manager and AWS Load Balancer Controller

--- components/eks-setup/eu-central-1-dev/main.tf
+++ components/eks-setup/eu-central-1-dev/main.tf
@@ -14,10 +14,10 @@ module "base" {
   environment = var.environment
   project     = var.project
 
-  eks_cluster_endpoint                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
-  eks_cluster_certificate_authority_data = data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data
-  eks_cluster_name                       = data.terraform_remote_state.eks.outputs.cluster_name
-  eks_cluster_oidc_issuer_url            = data.terraform_remote_state.eks.outputs.cluster_oidc_issuer_url
+  eks_cluster_endpoint                   = data.terraform_remote_state.eks.outputs.eks_cluster_endpoint
+  eks_cluster_certificate_authority_data = data.terraform_remote_state.eks.outputs.eks_cluster_certificate_authority_data
+  eks_cluster_name                       = data.terraform_remote_state.eks.outputs.eks_cluster_name
+  eks_cluster_oidc_issuer_url            = data.terraform_remote_state.eks.outputs.eks_cluster_oidc_issuer_url
 
   route53_hosted_zone_id = "Z01546261NTTOBINL68WG"
 }
