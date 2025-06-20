5bfe287020ea9309cea18837e04f10f3dac52350
ValentinLutz <allround.email@gmail.com>
2025-06-20 16:22:38 +0200

    feat: add remote state data source for VPC in Terraform configuration

--- components/eks/eu-central-1-dev/main.tf
+++ components/eks/eu-central-1-dev/main.tf
@@ -1,3 +1,12 @@
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "${var.project}-${var.region}-${var.environment}-iac-tofu-state"
+    key    = "vpc/terraform.tfstate"
+    region = var.region
+  }
+}
+
 module "base" {
   source = "../base"
 
@@ -5,12 +14,8 @@ module "base" {
   environment = var.environment
   project     = var.project
 
-  eks_version = "1.33"
-  eks_subnet_ids = [
-    "subnet-048f9754642b3e67c",
-    "subnet-091f240957beae8b8",
-    "subnet-0aa9585979927b512"
-  ]
+  eks_version    = "1.33"
+  eks_subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
   eks_cluster_admin_principal_arns = [
     "arn:aws:iam::387105013966:role/aws-reserved/sso.amazonaws.com/eu-central-1/AWSReservedSSO_admin_dc1b034f828d678b",
   ]
