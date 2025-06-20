06f1bdce6e5caf9970f051df7c6464534e40f301
ValentinLutz <allround.email@gmail.com>
2025-06-20 15:04:57 +0200

    fix: update helm provider configuration syntax

--- components/eks-setup/base/providers.tf
+++ components/eks-setup/base/providers.tf
@@ -44,11 +44,11 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = var.eks_cluster_endpoint
     cluster_ca_certificate = base64decode(var.eks_cluster_certificate_authority_data)
 
-    exec {
+    exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
       # This requires the awscli to be installed locally where Terraform is executed
