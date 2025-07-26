5b05785ef18a688f1c4713f421f998ab6f64d913
ValentinLutz <allround.email@gmail.com>
2025-07-26 15:42:48 +0200

    feat: disable DockerHub, GitHub, and GitLab pull-through cache in ECR configuration

--- components/ecr-pull-though-cache/eu-central-1-dev/main.tf
+++ components/ecr-pull-though-cache/eu-central-1-dev/main.tf
@@ -5,8 +5,8 @@ module "base" {
   environment = var.environment
   project     = var.project
 
-  enable_dockerhub_pull_through_cache = true
-  enable_github_pull_through_cache    = true
-  enable_gitlab_pull_through_cache    = true
+  enable_dockerhub_pull_through_cache = false
+  enable_github_pull_through_cache    = false
+  enable_gitlab_pull_through_cache    = false
   enable_quay_pull_through_cache      = true
 }
