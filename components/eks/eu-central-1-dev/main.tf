5c2f361c1885809bddae2bad8a1acffd888dd09f
ValentinLutz <allround.email@gmail.com>
2025-11-07 18:59:04 +0100

    feat: upgrade EKS version to 1.34

--- components/eks/eu-central-1-dev/main.tf
+++ components/eks/eu-central-1-dev/main.tf
@@ -14,7 +14,7 @@ module "base" {
   environment = var.environment
   project     = var.project
 
-  eks_version    = "1.33"
+  eks_version    = "1.34"
   eks_subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
   eks_cluster_admin_principal_arns = [
     "arn:aws:iam::387105013966:role/aws-reserved/sso.amazonaws.com/eu-central-1/AWSReservedSSO_admin_dc1b034f828d678b",
