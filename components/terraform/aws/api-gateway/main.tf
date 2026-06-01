55e175215462ba3dd2e49d0e8f921bae4d381196
ValentinLutz <allround.email@gmail.com>
2026-06-01 18:39:45 +0200

    refactor: source aws/api-gateway cert/dns/cognito refs dynamically

--- components/terraform/aws/api-gateway/main.tf
+++ components/terraform/aws/api-gateway/main.tf
@@ -7,14 +7,30 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
-# tflint-ignore: terraform_unused_declarations
-data "terraform_remote_state" "ssl_dns" {
+data "terraform_remote_state" "private_domain" {
   backend = "s3"
   config = {
     bucket = "${var.project}-${var.region}-${var.environment}-iac-tofu-state"
-    key    = "ssl-dns/terraform.tfstate"
+    key    = "private-domain/terraform.tfstate"
     region = var.region
   }
+}
+
+data "terraform_remote_state" "cognito" {
+  backend = "s3"
+  config = {
+    bucket = "${var.project}-${var.region}-${var.environment}-iac-tofu-state"
+    key    = "cognito/terraform.tfstate"
+    region = var.region
+  }
+}
+
+# The ACM certificate for the wildcard domain is managed outside this repo; look it up
+# by domain so a rebuild resolves the current ARN instead of a hardcoded one.
+data "aws_acm_certificate" "api" {
+  domain      = "*.${var.environment}.${var.region}.monke.science"
+  statuses    = ["ISSUED"]
+  most_recent = true
 }
 
 data "aws_lb" "k8s_api_gateway" {
@@ -39,8 +55,8 @@ module "api-gateway" {
   vpc_cidr_blocks             = data.terraform_remote_state.vpc.outputs.private_subnet_cidr_blocks
   vpc_id                      = data.terraform_remote_state.vpc.outputs.vpc_id
   load_balancer_arn           = data.aws_lb_listener.k8s_api_gateway_listener.arn
-  acm_cert_arn                = var.acm_cert_arn
-  route53_zone_id             = var.route53_zone_id
-  cognito_user_pool_endpoint  = var.cognito_user_pool_endpoint
-  cognito_user_pool_client_id = var.cognito_user_pool_client_id
+  acm_cert_arn                = data.aws_acm_certificate.api.arn
+  route53_zone_id             = data.terraform_remote_state.private_domain.outputs.route53_zone_id
+  cognito_user_pool_endpoint  = data.terraform_remote_state.cognito.outputs.cognito_user_pool_endpoint
+  cognito_user_pool_client_id = data.terraform_remote_state.cognito.outputs.cognito_user_pool_client_id
 }
--- components/terraform/aws/api-gateway/variables.tf
+++ components/terraform/aws/api-gateway/variables.tf
@@ -14,26 +14,6 @@ variable "component" {
   type = string
 }
 
-variable "acm_cert_arn" {
-  description = "ACM Certificate ARN for the domain"
-  type        = string
-}
-
-variable "route53_zone_id" {
-  description = "Route53 Hosted Zone ID for the domain"
-  type        = string
-}
-
-variable "cognito_user_pool_endpoint" {
-  description = "Cognito User Pool endpoint for authentication"
-  type        = string
-}
-
-variable "cognito_user_pool_client_id" {
-  description = "Cognito User Pool Client ID for authentication"
-  type        = string
-}
-
 # tflint-ignore: terraform_unused_declarations
 variable "namespace" {
   type    = string
